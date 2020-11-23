15bc9aa93891d6c4841cf471b5bbc1fc972206ef
Ntuthuko Mpaku <ntuthuko2@live.com>
2020-11-23 12:19:36 +0200

    map and mapview setup complete. all lifecyles handled

--- app/src/main/java/com/runtracker/BaseApplication.kt
+++ app/src/main/java/com/runtracker/BaseApplication.kt
@@ -1,6 +1,7 @@
 package com.runtracker
 
 import android.app.Application
+import com.huawei.hms.maps.MapsInitializer
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,6 +10,8 @@ class BaseApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // Setting Huawei Map API Key
+        MapsInitializer.setApiKey("CgB6e3x92FZHmP2/m7mdrL53fEkRNCSCfbFg9QhGJx0anhD/t4bE+UltlNcMGnvJOT+Cyejo1+U4wtfxRzPPke+Y")
 
         // Enables logging with the timber library
         Timber.plant(Timber.DebugTree())
--- app/src/main/java/com/runtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/runtracker/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,80 @@
 package com.runtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.huawei.hms.maps.HuaweiMap
+import com.huawei.hms.maps.MapView
+import com.huawei.hms.maps.MapsInitializer
+import com.huawei.hms.maps.OnMapReadyCallback
 import com.runtracker.R
 import com.runtracker.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TrackingFragment: Fragment(R.layout.fragment_tracking) {
+class TrackingFragment: Fragment(R.layout.fragment_tracking) , OnMapReadyCallback {
 
     private val viewModel: MainViewModel by viewModels()
+
+    private var huaweiMap: HuaweiMap? = null
+    private var huaweiMapView: MapView? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        huaweiMapView = view.findViewById(R.id.mapView)
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey")
+        }
+        huaweiMapView?.onCreate(mapViewBundle)
+        huaweiMapView?.getMapAsync(this)
+    }
+
+    override fun onMapReady(hMap: HuaweiMap?) {
+            huaweiMap = hMap
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        var mapViewBundle: Bundle? = outState.getBundle("MapViewBundleKey")
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle("MapViewBundleKey", mapViewBundle)
+        }
+
+        huaweiMapView?.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        huaweiMapView?.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        huaweiMapView?.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        huaweiMapView?.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        huaweiMapView?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        huaweiMapView?.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        huaweiMapView?.onLowMemory()
+    }
 }
